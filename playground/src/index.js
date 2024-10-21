40a49af0463ace1d5bd7fc0b82612ec229d7897c
fuhao <632951357@qq.com>
2024-10-21 11:26:02 +0800

    feat: 编写测试Demo

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,8 +1,24 @@
-import './index.css';
+import { createApp, h } from 'vue';
+import SVGDefault from './test.svg';
+import SVGComponent from './test.svg?Component';
+import SVGRaw from './test.svg?raw';
+import SVGUrl from './test.svg?url';
 
-document.querySelector('#root').innerHTML = `
-<div class="content">
-  <h1>Vanilla Rsbuild</h1>
-  <p>Start building amazing things with Rsbuild.</p>
-</div>
-`;
+console.log('SVGDefault', SVGDefault);
+console.log('SVGUrl', SVGUrl);
+console.log('SVGRaw', SVGRaw);
+console.log('SVGComponent', SVGComponent);
+
+const App = () => {
+  return h('div', [
+    h('div', [h('h1', 'SVG Default'), h(SVGDefault)]),
+    h('div', [h('h1', 'SVG Url'), h('img', { src: SVGUrl })]),
+    h('div', [h('h1', 'SVG Raw'), h('div', { innerHTML: SVGRaw })]),
+    h('div', [h('h1', 'SVG Component'), h(SVGComponent)]),
+  ]);
+};
+
+const app = createApp(App());
+
+document.querySelector('#root').innerHTML = '<div id="app"></div>';
+app.mount('#app');
